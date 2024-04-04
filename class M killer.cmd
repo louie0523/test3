5bb581087ee3961fb74d9d67db7e0261c05864b0
louie0523 <151519353+louie0523@users.noreply.github.com>
2024-04-04 10:54:00 +0900

    Add files via upload

---
+++ class M killer.cmd
@@ -0,0 +1,23 @@
+set file=.\n1.mp3
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >n1.vbs
+start /min n1.vbs
+@echo off
+:loop
+color 2
+set /a a+=1
+color 2
+mode 1000
+echo Hacked Process!
+echo Killed ClassM Process!
+taskkill -F -T /PID ClassM_Client.exe
+taskkill -F -T /PID ClassM_Client_Service.exe
+taskkill -F -T /PID SysCtrl.exe
+taskkill -F -T /PID mvnc.exe
+cls
+goto loop
